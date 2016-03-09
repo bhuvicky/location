74fad6fc77ddd39947046c05bf8539f7793463e5
bhuvanesh <ssbhuvaneshwar@gmail.com>
2016-03-09 16:06:34 +0530

    changes in country activity by bhuvanesh, commit through studio

--- app/src/main/java/com/healthsignz/locationinfo/CountryActivity.java
+++ app/src/main/java/com/healthsignz/locationinfo/CountryActivity.java
@@ -1,13 +1,14 @@
 package com.healthsignz.locationinfo;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+public class CountryActivity extends AppCompatActivity {
 
     Spinner mSpinnerCountryName;
     boolean firstTime = true;
@@ -17,17 +18,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_country);
         mSpinnerCountryName = (Spinner) findViewById(R.id.spinner_country_name);
+        mSpinnerCountryName.setBackgroundColor(Color.BLUE);
         mSpinnerCountryName.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 if(!firstTime) {
                     String countryName = parent.getItemAtPosition(position).toString();
-                    Intent countryIntent = new Intent(MainActivity.this, StateActivity.class);
+                    Intent countryIntent = new Intent(CountryActivity.this, StateActivity.class);
                     countryIntent.putExtra("CountrySelected", countryName);
                     startActivity(countryIntent);
                 }
 
-                firstTime = false ;
+                firstTime = false;
             }
 
             @Override
